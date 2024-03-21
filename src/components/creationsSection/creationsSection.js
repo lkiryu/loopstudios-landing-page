f96f690c345114f0f8c68a585b0551ecdaf0bb11
Kiryu <13matheusilva@gmail.com>
2024-03-21 20:56:17 -0300

    card style add

--- src/components/creationsSection/creationsSection.js
+++ src/components/creationsSection/creationsSection.js
@@ -1,5 +1,12 @@
 import React from "react";
-import Card from "../card/card";
+import deepEarth from "../../images/desktop/image-deep-earth.jpg"
+import nightArcade from "../../images/desktop/image-night-arcade.jpg"
+import soccerTeam from "../../images/desktop/image-soccer-team.jpg"
+import grid from "../../images/desktop/image-grid.jpg"
+import fromAbove from "../../images/desktop/image-from-above.jpg"
+import pocketBorealis from "../../images/desktop/image-pocket-borealis.jpg"
+import curiosity from "../../images/desktop/image-curiosity.jpg"
+import fisheye from "../../images/desktop/image-fisheye.jpg"
 
 import './creationsSection.css'
 
@@ -11,14 +18,38 @@ const creationsSection = () => {
                 <a className="button" href="/">See all</a>
             </div>
             <div className="cards">
-                <Card title='Deep earth' />
-                <Card title='Night arcade' />
-                <Card title='Soccer team VR' />
-                <Card title='The grid' />
-                <Card title='From up above VR' />
-                <Card title='Pocket borealis' />
-                <Card title='The curiosity' />
-                <Card title='Make it fisheye' />
+                <div className="card">
+                    <img src={deepEarth} alt="background"></img>
+                    <h2 className="title">Deep earth</h2>
+                </div>
+                <div className="card">
+                    <img src={nightArcade} alt="background"></img>
+                    <h2 className="title">Night arcade</h2>
+                </div>
+                <div className="card">
+                    <img src={soccerTeam} alt="background"></img>
+                    <h2 className="title">Soccer team VR</h2>
+                </div>
+                <div className="card">
+                    <img src={grid} alt="background"></img>
+                    <h2 className="title">The grid</h2>
+                </div>
+                <div className="card">
+                    <img src={fromAbove} alt="background"></img>
+                    <h2 className="title">From up above VR</h2>
+                </div>
+                <div className="card">
+                    <img src={pocketBorealis} alt="background"></img>
+                    <h2 className="title">Pocket borealis</h2>
+                </div>
+                <div className="card">
+                    <img src={curiosity} alt="background"></img>
+                    <h2 className="title">The curiosity</h2>
+                </div>
+                <div className="card">
+                    <img src={fisheye} alt="background"></img>
+                    <h2 className="title">Make it fisheye</h2>
+                </div>
             </div>
         </section>
     )
