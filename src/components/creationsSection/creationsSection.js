c02b1d231a1b296b90c4ac6b6d7e4ba6e57e89db
Kiryu <13matheusilva@gmail.com>
2024-03-21 18:06:45 -0300

    card responsive add

--- src/components/creationsSection/creationsSection.js
+++ src/components/creationsSection/creationsSection.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Card from "../card/card";
 
 const creationsSection = () => {
     return (
         <section className="creations">
             <h2 className="title">Our creations</h2>
             <a className="button" href="#">See all</a>
-            <div className="cards"></div>
+            <div className="cards">
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+                <Card title=''/>
+            </div>
         </section>
     )
 }
