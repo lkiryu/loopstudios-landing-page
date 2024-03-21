b669804635959b9a0579d8c6a34d7a0d7653823e
Kiryu <13matheusilva@gmail.com>
2024-03-21 20:00:13 -0300

    creations section style add

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
     return(
         <div className="card">
-            <h2 className="cardTitle">{props.title}</h2>
+            <h2 className="title">{props.title}</h2>
         </div>
     )
 }
--- src/components/creationsSection/creationsSection.js
+++ src/components/creationsSection/creationsSection.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Card from "../card/card";
 
+import './creationsSection.css'
+
 const creationsSection = () => {
     return (
         <section className="creations">
-            <h2 className="title">Our creations</h2>
-            <a className="button" href="/">See all</a>
+            <div className="creationsHeader">
+                <h2 className="title">Our creations</h2>
+                <a className="button" href="/">See all</a>
+            </div>
             <div className="cards">
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
-                <Card title=''/>
+                <Card title='Deep earth' />
+                <Card title='Night arcade' />
+                <Card title='Soccer team VR' />
+                <Card title='The grid' />
+                <Card title='From up above VR' />
+                <Card title='Pocket borealis' />
+                <Card title='The curiosity' />
+                <Card title='Make it fisheye' />
             </div>
         </section>
     )
