43e421c9b1c91ee3dd4384072930dcf6200fe9d2
Kiryu <13matheusilva@gmail.com>
2024-03-21 21:58:48 -0300

    footer style add

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,51 +1,53 @@
 import React from "react";
 import logo from '../images/logo.svg'
 
+import './footer.css'
+
 const Footer = () => {
-    return(
+    return (
         <footer className="footer">
-                <div>
-                    <a href="/">
-                        <img src={logo} alt="logo"></img>
-                    </a>
+            <div>
+                <a href="/">
+                    <img src={logo} alt="logo"></img>
+                </a>
 
-                    <ul className="social">
-                        <li>
-                            <a href="/"><i class="fa-brands fa-facebook"></i></a>
-                        </li>
-                        <li>
-                            <a href="/"><i class="fa-brands fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="/"><i class="fa-brands fa-pinterest"></i></a>
-                        </li>
-                        <li>
-                            <a href="/"><i class="fa-brands fa-instagram"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <a href="/">About</a>
-                        </li>
-                        <li>
-                            <a href="/">Careers</a>
-                        </li>
-                        <li>
-                            <a href="/">Events</a>
-                        </li>
-                        <li>
-                            <a href="/">Products</a>
-                        </li>
-                        <li>
-                            <a href="/">Support</a>
-                        </li>
-                    </ul>
+                <ul className="social">
+                    <li>
+                        <a href="/"><i className="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="/"><i className="fa-brands fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="/"><i className="fa-brands fa-pinterest"></i></a>
+                    </li>
+                    <li>
+                        <a href="/"><i className="fa-brands fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">About</a>
+                    </li>
+                    <li>
+                        <a href="/">Careers</a>
+                    </li>
+                    <li>
+                        <a href="/">Events</a>
+                    </li>
+                    <li>
+                        <a href="/">Products</a>
+                    </li>
+                    <li>
+                        <a href="/">Support</a>
+                    </li>
+                </ul>
 
-                    <p className="rights">© 2021 Loopstudios. All rights reserved.</p>
-                </div>
-            </footer>
+                <p className="rights">© 2021 Loopstudios. All rights reserved.</p>
+            </div>
+        </footer>
     )
 }
 
